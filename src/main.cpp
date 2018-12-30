88cd77bac100eb8d04ee834c9206a284879b8e46
Justin Meyer <justin@practicus.io>
2018-12-30 18:41:54 -0500

    Working back up to moving

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,49 @@
 
 Serial pc (SERIAL_TX, SERIAL_RX);
 
-int period = 50; // Period @ 20khz
+int period = 50; // Period @ 20KHZ
+
+// TIM1
+// PA8
+// PA9
+
+DigitalIn top_limit(D3, PullUp); // Top Limit Switch
+DigitalIn bottom_limit(D5, PullUp); // Bottom Limit Switch (Prime)
+DigitalIn start_stop(D11, PullUp); // Emergency Stop (Should be paired to relay/trans out)
+
+// LPD3806 600BM G5 24C
+// Red...5-24VDC...
+// Black...Ground...
+// Green...A Phase...
+// White...B Phase...
+// Must be pulled up
+
+// Pololu Dual VNH5019 Motor Driver Shield for Arduino
+// Driver 1
+DigitalOut M1INA(D2); // Clockwise Input
+DigitalOut M1INB(D4); // Counter-Clockwise Input
+DigitalIn M1EN_DIAG(D6); // High = Half-Bridge A & B Enabled, Low = Half-Bridge A & B Disabled (Combination of VHN5019 ENA/DIAGA & ENB/DIAGA Pins)
+PwmOut M1PWM(D9); // PWM Input, 20KHZ Max, Low = Off, High = On, Dependent On xINA/B Pins
+AnalogIn M1CS(A0); // Output Of Current Sense Proportional To Motor Current If CS_DIS Is Low Or Open (CD_DIS Not Connected)
 
 int main() {
 
     int i = 1; // Keep loop running unless shit goes south
 
+    // Turn off direction inputs
+    M1INA.write(0);
+    M1INB.write(0);
+
+    // Setup PWM
+    M1PWM.period_us(50); // Set to 20KHZ
+    M1PWM.write(0.2f); // Set Duty Cycle to 20%
+
     while(i) {
+        M1INA = top_limit;
+        M1INB = bottom_limit;
+
+        pc.printf("top: %d, bottom: %d, other: %d\n", top_limit, bottom_limit, start_stop);
 
         wait_us(period); // Period
-
     }
 }
