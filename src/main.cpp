74cc56a43a188898056a558bf0391b6e78d73164
Justin Meyer <justin@practicus.io>
2018-12-30 16:09:53 -0500

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 
 Serial pc (SERIAL_TX, SERIAL_RX);
 
-
+int period = 50; // Period @ 20khz
 
 int main() {
+
+    int i = 1; // Keep loop running unless shit goes south
 
     while(i) {
 
         wait_us(period); // Period
+
     }
 }
